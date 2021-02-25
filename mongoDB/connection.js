b6e3aea83b886f7f0b097a42a2d44fdbca66bb97
shawn220a <shawn.42496@gmail.com>
2021-02-25 16:44:50 -0600

    Add connection code for mongoDB

---
+++ mongoDB/connection.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('DB Connected...');
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+};
+
+const disconnect = async () => {
+  try {
+    await mongoose.disconnect();
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+};
+
+const dropCollection = async (collectionName) => {
+  try {
+    await mongoose.collection(collectionName).drop();
+  } catch (err) {
+    if (err.code === 26) {
+      console.log('namespace %s not found', collectionName);
+    } else {
+      throw new Error(err);
+    }
+  }
+};
+
+module.exports = { connect, disconnect, dropCollection };
