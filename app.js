008fce1fa81ecae6f472fa9afabe18366f3cb4d6
shawn220a <shawn.42496@gmail.com>
2021-02-25 16:41:33 -0600

    Add express structure to app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const formRoutes = require('./routes/forms.js');
+const db = require('./mongoDB/connection.js');
+
+const app = express();
+
+db.connect();
+
+// Body Parser Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+  res.send('Welcome to the backend');
+});
+
+app.use('/form', formRoutes);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server listening on: http://localhost:${port}`);
+});
