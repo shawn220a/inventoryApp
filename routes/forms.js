aa34c4258dbe038beb25314a732cb938c2832629
shawn220a <shawn.42496@gmail.com>
2021-02-25 17:05:59 -0600

    Create route for finding data in DB

--- routes/forms.js
+++ routes/forms.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const itemModel = require('../models/item.model.js');
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('Form route'));
+
+router.get('/', async (req, res) => {
+  try {
+    const allResults = await itemModel.find({});
+    if (allResults && allResults.length > 0)
+      res.status(200).send('/UpdateItem');
+    else res.status(404).json();
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
