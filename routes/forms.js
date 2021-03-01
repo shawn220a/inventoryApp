ef6f33efcb5d9a485a5a0a2b4cd462c464f18c95
shawn220a <shawn.42496@gmail.com>
2021-03-01 12:40:02 -0600

    Update get route for inventory

--- routes/forms.js
+++ routes/forms.js
@@ -2,13 +2,11 @@ const express = require('express');
 const itemModel = require('../models/item.model.js');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Form route'));
-
-router.get('/one', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const allResults = await itemModel.find({});
     if (allResults && allResults.length > 0) res.status(200).send(allResults);
-    else res.status(200).send('/NewItem');
+    else res.status(404).json();
   } catch (err) {
     res.status(500).json(err);
   }
